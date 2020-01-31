7574f9f702745f185f5f0bbab6331978c3189b00
中山一仁 <firstjin19990101@gmail.com>
2020-01-31 22:56:56 +0900

    Having finished adding column "name" to User Model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
+
+    # ==========ここから編集する==========
+    protect_from_forgery with: :exception
+
+    # 以下のコードにより、全てのページでログインしていないユーザーはdeviseのログイン画面に飛ばされる
     before_action :authenticate_user!
-    
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    # nameカラムをdeviseに追加
+    protected
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+            devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+        end
+    # ==========ここまで編集する==========
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # ユーザーの新規登録をする際に、ユーザー名が何もない状態だと困るので、nameカラムにバリデーションを設定します
+  validates :name, presence: true, length: { maximum: 20 }
 end
