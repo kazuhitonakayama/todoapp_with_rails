be0cf6f27d679d3d495bb4653d80142d5e7db548
中山一仁 <firstjin19990101@gmail.com>
2020-02-02 19:26:02 +0900

    Having finished making ”Card Model and Setting Association”

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,9 @@
 class List < ApplicationRecord
-    # LIstは一人のユーザに依存するよという命令
+    # Listは一人のユーザに依存するよという命令
     belongs_to :user
+
+    # Listは複数のカードを持つようにという命令
+    has_many :cards, dependent: :destroy
 
     #titleは1文字以上255文字以内にしてねという命令
     validates :title, length: { in: 1..255 }
