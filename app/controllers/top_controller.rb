ee67fa3de451694f554229cf54f64dccb6a6a14c
中山一仁 <firstjin19990101@gmail.com>
2020-02-03 17:27:08 +0900

    Having finished UI fixing

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
-    # 新規作成したListの一覧を表示
+    # 新規作成したListの一覧を表示　なおかつ、ログインユーザーが投稿した投稿のみ表示するようにする
     @lists = List.where(user: current_user).order("created_at ASC")
   end
 end
