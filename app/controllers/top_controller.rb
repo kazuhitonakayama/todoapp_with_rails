cf348c6e7d299fa5dfa4a37a680620f2264e881a
中山一仁 <firstjin19990101@gmail.com>
2020-02-10 19:49:11 +0900

    trivial fixing

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,7 @@
 class TopController < ApplicationController
   def index
     # 新規作成したListの一覧を表示　なおかつ、ログインユーザーが投稿した投稿のみ表示するようにする
+    # find_byとwhereの違い https://qiita.com/tsuchinoko_run/items/f3926caaec461cfa1ca3
     @lists = List.where(user: current_user).order("created_at ASC")
   end
 end
